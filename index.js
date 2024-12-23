8df0d312c29b7bdf50476ae9561ce461aa7a3a2a
holysama <97112425+holysama@users.noreply.github.com>
2024-12-23 17:05:09 +0100

    style: add number 0 button

--- index.js
+++ index.js
@@ -11,8 +11,47 @@ let multiply = function (a, b) {
 }
 
 let divide = function(a, b) {
-
     return a / b;
 }
 
-let operate = 
+let operation = function(operator, a, b) {
+    return;
+}
+
+let numbersContainer = document.querySelector(".numbers-container");
+let display = document.querySelector(".display");
+
+numbersContainer.addEventListener("click", (event) => {
+
+    switch(event.target.classList) {
+        case "zero":
+            display.textContent = 0;
+
+        case "one":
+            display.textContent = 1;
+
+        case "two":
+            display.textContent = 2;
+
+        case "three":
+            display.textContent = 3;
+
+        case "four":
+            display.textContent = 4;
+
+        case "five":
+            display.textContent = 5;
+
+        case "six":
+            display.textContent = 6;
+
+        case "seven":
+            display.textContent = 7;
+
+        case "eight":
+            display.textContent = 8;
+
+        case "nine":
+            display.textContent = 9;    
+    }
+})
